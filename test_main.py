bc0a5336a92f024614b2923e08e24040b99e8dac
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2026-04-08 00:03:53 +0300

    product shop with file data.txt

--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 import main
 
 ##def test_first():
- #   assert main.hello("Warld") == "Hello Warld!"
- #   assert main.hello("W7rld") == "Hello W7rld!"
+#   assert main.hello("Warld") == "Hello Warld!"
+#   assert main.hello("W7rld") == "Hello W7rld!"
