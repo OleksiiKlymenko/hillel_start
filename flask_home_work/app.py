c3fa77b063f4f45d6915282ee4b16ca2ecddab47
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2026-04-15 13:05:11 +0300

    apps about f1 drivers, v1.2

--- flask_home_work/app.py
+++ flask_home_work/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", result = "This is the main page")
+    return render_template("index.html", result = "Це основна сторінка")
 
 @app.route("/f1_teams")
 def list_f1_teams():
@@ -27,4 +27,25 @@ def drivers(name):
     if found_driver:
         return render_template("driver_info.html", driver=found_driver)
 
-app.run(debug=True)    # запускаємо локальний сервер
+
+@app.route("/your_vote")
+def your_vote():
+    all_teams = generate_teams()
+    return render_template("your_vote.html", teams=all_teams)
+
+
+@app.route("/check_winner", methods=["POST"])
+def check_winner():
+    user_team = request.form.get("selected_team")
+    user_driver = request.form.get("selected_driver")
+
+    ferrari_drivers = ["Lewis Hamilton", "Charles Leclerc"]
+
+    is_ferrari = ("Ferrari" in user_team) and (user_driver in ferrari_drivers)
+
+    return render_template("result.html",
+                           team=user_team,
+                           driver=user_driver,
+                           is_ferrari=is_ferrari)
+
+app.run(debug=True)
